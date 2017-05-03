c2268d3c2b1faa59b90157b7e1b3a8925cc2370d
kenNg1 <kng1230@hotmail.com>
2017-05-03 13:35:27 +0800

    add signin upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to Ken's Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -54,5 +55,7 @@ class UsersController < ApplicationController
     if current_user != @user
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
+    end
   end
+  
 end
